369756c027ee4a43eeca5e6f3312a006a609fc62
marlom46 <80200977+marlom46@users.noreply.github.com>
2021-06-23 23:05:03 +0530

    Done Compiling message.sol

---
+++ Message/compile.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+const messagePath = path.resolve(__dirname, 'contracts', 'message.sol');
+const source = fs.readFileSync(messagePath, 'utf-8');
+
+const input = {
+
+    language: "Solidity",
+    sources: {
+        "message.sol": {
+            content: source,
+        },
+    },
+    settings: {
+        outputSelection: {
+            "*": {
+                "*": ["*"],
+            },
+        },
+    },
+};
+
+const output = solc.compile(JSON.stringify(input));
+console.log(output);
---
+++ Message/contracts/message.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity =0.8.6;
+
+contract Inbox {
+    string public message;
+
+    constructor(string memory _initialMessage) {
+        message = _initialMessage;
+    }
+
+    function setMessage(string memory _newMessage) public {
+        message = _newMessage;
+    }
+}
